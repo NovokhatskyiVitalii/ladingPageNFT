340b372ff4691379c888cfb5f7c90bf398d9a2ab
Vitalii <tomrain78@gmail.com>
2023-07-12 10:02:05 +0300

    adding section categories

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -27,13 +27,27 @@ const swiper = new Swiper(".auctions-slider", {
 
 const swiper2 = new Swiper(".popular-slider", {
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
   },
   modules: [Navigation, EffectFade, Pagination, FreeMode],
   autoHeight: true,
   speed: 500,
   slidesPerView: 2.5,
+  spaceBetween: 42,
+  freeMode: true,
+  grabCursor: true,
+});
+
+const swiper3 = new Swiper(".categories-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+  modules: [Navigation, EffectFade, Pagination, FreeMode],
+  autoHeight: true,
+  speed: 500,
+  slidesPerView: 3,
   spaceBetween: 42,
   freeMode: true,
   grabCursor: true,
