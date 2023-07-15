1c2ecf78c4392a44ac7fdcf04aa4d96966b89ba7
Vitalii <tomrain78@gmail.com>
2023-07-15 15:01:57 +0300

    adding adaptive for tablet and mobile

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -11,44 +11,103 @@ import Swiper, {
 } from "swiper";
 import "swiper/css/bundle";
 
-const swiper = new Swiper(".auctions-slider", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  modules: [Navigation, EffectFade, Pagination, FreeMode],
-  autoHeight: true,
-  speed: 500,
-  slidesPerView: 3,
-  spaceBetween: 42,
-  freeMode: true,
-  grabCursor: true,
-});
+const swiper = () => {
+  new Swiper(".auctions-slider", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, EffectFade, Pagination, FreeMode],
+    autoHeight: true,
+    speed: 500,
+    slidesPerView: 3,
+    spaceBetween: 15,
+    freeMode: true,
+    grabCursor: true,
+    breakpoints: {
+      1300: {
+        spaceBetween: 42,
+      },
+      1100: {
+        slidesPerView: 3,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+    },
+  });
+};
 
-const swiper2 = new Swiper(".popular-slider", {
-  navigation: {
-    nextEl: ".swiper-button-next1",
-    prevEl: ".swiper-button-prev1",
-  },
-  modules: [Navigation, EffectFade, Pagination, FreeMode],
-  autoHeight: true,
-  speed: 500,
-  slidesPerView: 2.5,
-  spaceBetween: 42,
-  freeMode: true,
-  grabCursor: true,
-});
+const swiper2 = () => {
+  new Swiper(".popular-slider", {
+    navigation: {
+      nextEl: ".swiper-button-next1",
+      prevEl: ".swiper-button-prev1",
+    },
+    modules: [Navigation, EffectFade, Pagination, FreeMode],
+    autoHeight: true,
+    speed: 500,
+    slidesPerView: 2.5,
+    freeMode: true,
+    grabCursor: true,
+    spaceBetween: 15,
+    freeMode: true,
+    grabCursor: true,
+    breakpoints: {
+      1300: {
+        spaceBetween: 42,
+      },
+      1100: {
+        slidesPerView: 2.5,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+    },
+  });
+};
+const swiper3 = () => {
+  new Swiper(".categories-slider", {
+    navigation: {
+      nextEl: ".swiper-button-next2",
+      prevEl: ".swiper-button-prev2",
+    },
+    modules: [Navigation, EffectFade, Pagination, FreeMode],
+    autoHeight: true,
+    speed: 500,
+    slidesPerView: 3,
+    spaceBetween: 42,
+    freeMode: true,
+    grabCursor: true,
+    spaceBetween: 15,
+    freeMode: true,
+    grabCursor: true,
+    breakpoints: {
+      1300: {
+        spaceBetween: 42,
+      },
+      1100: {
+        slidesPerView: 3,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+    },
+  });
+};
 
-const swiper3 = new Swiper(".categories-slider", {
-  navigation: {
-    nextEl: ".swiper-button-next2",
-    prevEl: ".swiper-button-prev2",
-  },
-  modules: [Navigation, EffectFade, Pagination, FreeMode],
-  autoHeight: true,
-  speed: 500,
-  slidesPerView: 3,
-  spaceBetween: 42,
-  freeMode: true,
-  grabCursor: true,
-});
+function detectDevice() {
+  if (window.innerWidth < 600) {
+    for (let i = 0; i <= 3; i++) {
+      document.querySelectorAll(".auctions-slide")[i].style.display = "block";
+      document.querySelectorAll(".popular-slide")[i].style.display = "block";
+      document.querySelectorAll(".categories-slide")[i].style.display = "block";
+    }
+  } else {
+    swiper();
+    swiper2();
+    swiper3();
+  }
+}
+
+detectDevice();
